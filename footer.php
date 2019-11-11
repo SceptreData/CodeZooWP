2a407ea002ce850eaa7dfd2614eddfc663eaaab6
David Bergeron <david.f.bergeron@gmail.com>
2019-11-11 01:51:13 -0700

    work work

--- footer.php
+++ footer.php
@@ -14,19 +14,35 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'codezoo' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'codezoo' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'codezoo' ), 'codezoo', '<a href="http://www.davidbergeron.ca">David Bergeron</a>' );
-				?>
-		</div><!-- .site-info -->
+		<aside class="footer-content fit-content">
+			<section>
+				<h5>Discover Jabber </h5>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi libero, quas sequi odit voluptate voluptas ducimus non doloremque aperiam repudiandae beatae perspiciatis suscipit assumenda neque. Natus nihil porro doloribus autem?</p>
+			</section>
+
+			<section>
+				<h5>Discover Jabber </h5>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi libero, quas sequi odit voluptate voluptas ducimus non doloremque aperiam repudiandae beatae perspiciatis suscipit assumenda neque. Natus nihil porro doloribus autem?</p>
+			</section>
+
+			<section>
+				<h5>Discover Jabber </h5>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi libero, quas sequi odit voluptate voluptas ducimus non doloremque aperiam repudiandae beatae perspiciatis suscipit assumenda neque. Natus nihil porro doloribus autem?</p>
+			</section>
+</aside>
+		<nav class="footer-nav fit-content">
+			<div class="brand">
+				<a href="/">
+					<svg class="svg-inline--fa fa-hippo fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M581.12 96.2c-27.67-.15-52.5 17.58-76.6 26.62C489.98 88.27 455.83 64 416 64c-11.28 0-21.95 2.3-32 5.88V56c0-13.26-10.75-24-24-24h-16c-13.25 0-24 10.74-24 24v48.98C286.01 79.58 241.24 64 192 64 85.96 64 0 135.64 0 224v240c0 8.84 7.16 16 16 16h64c8.84 0 16-7.16 16-16v-70.79C128.35 407.57 166.72 416 208 416s79.65-8.43 112-22.79V464c0 8.84 7.16 16 16 16h64c8.84 0 16-7.16 16-16V288h128v32c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-32c17.67 0 32-14.33 32-32v-92.02c0-34.09-24.79-67.59-58.88-67.78zM448 176c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"></path></svg>
+					<h4>CodeZoo</h4>
+				</a>
+			</div>
+			<div class="footer-nav-links">
+				<a href="#">About</a>
+				<a href="#">Help</a>
+				<a href="#">Legal</a>
+			</div>
+		</nav><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
