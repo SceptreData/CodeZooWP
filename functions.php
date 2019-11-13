a15bfeae0dd86848b4d15835600117da24135c6c
David Bergeron <dbergeron2@nait.ca>
2019-11-13 10:56:09 -0700

    Oh boy I broke everything

--- functions.php
+++ functions.php
@@ -43,9 +43,7 @@ if ( ! function_exists( 'codezoo_setup' ) ) :
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'codezoo' ),
-		) );
+      	register_nav_menus(array('jargon-site' => __( 'Jargon Site Navigation' )));
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
@@ -163,11 +161,11 @@ add_action( 'widgets_init', 'codezoo_widgets_init' );
 function codezoo_scripts() {
   wp_enqueue_style( 'codezoo-style', get_stylesheet_uri() );
 
-  wp_enqueue_script( 'gutenberg-typography', get_template_directory_uri() . '/js/gutenberg.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'codezoo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'codezoo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'mobile-menu', get_template_directory_uri() . '/js/mobile-menu.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
